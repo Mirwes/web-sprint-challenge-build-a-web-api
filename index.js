4eca75ece6b22fae34c3775a04dd262cab6b627e
Mirwes <mirwes210@gmail.com>
2021-02-21 21:28:17 -0800

    actionRouter been imported to server.js and server is running on port localhost:4000

--- index.js
+++ index.js
@@ -12,4 +12,9 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
-const myName = 'Mirwes'
+
+const server = require('./server.js');
+
+server.listen(4000, () => {
+    console.log('\n* Server Running on http://localhost:4000 *\n');
+  });
